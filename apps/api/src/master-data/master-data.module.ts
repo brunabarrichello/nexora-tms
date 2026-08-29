b0ebbdb1d80e221b4800a8a635410d7bca1c846e
brunabarrichello <brunabarrichello2018@gmail.com>
2026-08-29 17:56:01 -0300

    NEX-27 feat(api): register business party directory

--- apps/api/src/master-data/master-data.module.ts
+++ apps/api/src/master-data/master-data.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { AuthenticationModule } from '../security/authentication.module.js';
 import { TenantRuntimeGateGuard } from '../tenant-runtime-gate.guard.js';
 import { TenancyModule } from '../tenancy/tenancy.module.js';
+import { BusinessPartyDirectoryController } from './business-party-directory.controller.js';
+import { BusinessPartyDirectoryService } from './business-party-directory.service.js';
 import { BusinessPartyController } from './business-party.controller.js';
 import { BusinessPartyService } from './business-party.service.js';
 
 @Module({
   imports: [AuthenticationModule, TenancyModule],
-  controllers: [BusinessPartyController],
-  providers: [BusinessPartyService, TenantRuntimeGateGuard],
+  controllers: [BusinessPartyController, BusinessPartyDirectoryController],
+  providers: [BusinessPartyService, BusinessPartyDirectoryService, TenantRuntimeGateGuard],
 })
 export class MasterDataModule {}
