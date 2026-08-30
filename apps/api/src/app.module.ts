39bcc3db12d4c03c9680c35c8950548fce26b08f
brunabarrichello <brunabarrichello2018@gmail.com>
2026-08-30 01:21:47 -0300

    feat(documents): register documents in API app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { ApiController } from './api.controller.js';
 import { CapacityModule } from './capacity/capacity.module.js';
+import { DocumentsModule } from './documents/documents.module.js';
 import { FreightModule } from './freight/freight.module.js';
 import { HealthController } from './health.controller.js';
 import { MasterDataModule } from './master-data/master-data.module.js';
@@ -21,6 +22,7 @@ import { TenancyModule } from './tenancy/tenancy.module.js';
     CapacityModule,
     MatchingModule,
     NegotiationModule,
+    DocumentsModule,
   ],
   controllers: [ApiController, HealthController],
 })
